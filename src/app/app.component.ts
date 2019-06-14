4e2c5dea96d914eb3fb8f9819dcb6529936a2bd8
Marc Davies <marcdaviesriot@gmail.com>
2019-06-14 14:20:29 -0700

    add hard coded list of shows in AppComponent class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,9 @@ import { Show } from './models/show.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentFocus: string = 'Angular Homework';
-  currentTime = new Date();
-  month: number = this.currentTime.getMonth() + 1;
-  day: number = this.currentTime.getDate();
-  year: number = this.currentTime.getFullYear();
   shows: Show[] = [
-    new Show('Finish weekend Angular homework for Epicodus course'),
-    new Show('Begin brainstorming possible JavaScript group projects'),
-    new Show('Add README file to last few Angular repos on GitHub')
+    new Show('[NOFX, Bad Religion]', 'Portland Meadows', '07/12/2019', '$45', '21+'),
+    new Show('[Nile, Terrorizer]', 'Hawthorne Theater', '11/22/2019', '$25', 'All Ages'),
+    new Show('[Gojira]', 'Roseland Theater', '07/29/2019', '$25', 'All Ages'),
   ];
 }
-

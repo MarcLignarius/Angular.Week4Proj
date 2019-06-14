f7943eda5edbf5ad884072ed90af19d7ea917fff
Marc Davies <marcdaviesriot@gmail.com>
2019-06-14 14:18:14 -0700

    refactor date property as string for now

--- src/app/models/show.model.ts
+++ src/app/models/show.model.ts
@@ -2,11 +2,8 @@ export class Show {
     constructor(
       public bands: string[],
       public venue: string,
-      public date: Date,
-      public month: number = date.getMonth(),
-      public day: number = date.getDay(),
-      public year: number = date.getFullYear(),
+      public date: string,
       public price: number,
-      public allAges: string,
+      public ageLimit: string,
       ) { }
   }

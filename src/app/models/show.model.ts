961a16cb4af09316dcb119e631f1f7cc9dd2e3ca
Marc Davies <marcdaviesriot@gmail.com>
2019-06-14 14:02:10 -0700

    create Show class along with constructor

--- src/app/models/show.model.ts
+++ src/app/models/show.model.ts
@@ -1,4 +1,12 @@
 export class Show {
-    public done: boolean = false;
-    constructor(public description: string) { }
+    constructor(
+      public bands: string[],
+      public venue: string,
+      public date: Date,
+      public month: number = date.getMonth(),
+      public day: number = date.getDay(),
+      public year: number = date.getFullYear(),
+      public price: number,
+      public allAges: string,
+      ) { }
   }

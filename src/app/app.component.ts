95d125710f693115aec33e2057da1e234cd364a3
Marc Davies <marcdaviesriot@gmail.com>
2019-06-14 14:45:04 -0700

    refactor hard coded list to fix compiling errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Show } from './models/show.model';
 })
 export class AppComponent {
   shows: Show[] = [
-    new Show('[NOFX, Bad Religion]', 'Portland Meadows', '07/12/2019', '$45', '21+'),
-    new Show('[Nile, Terrorizer]', 'Hawthorne Theater', '11/22/2019', '$25', 'All Ages'),
-    new Show('[Gojira]', 'Roseland Theater', '07/29/2019', '$25', 'All Ages'),
+    new Show(['NOFX, Bad Religion'], 'Portland Meadows', '07/12/2019 | 08:00 pm', 45, '21+'),
+    new Show(['Nile, Terrorizer'], 'Hawthorne Theater', '11/22/2019 | 07:30 pm', 25, 'All Ages'),
+    new Show(['Gojira'], 'Roseland Theater', '07/29/2019 | 08:30 pm', 25, 'All Ages'),
   ];
 }

503ce999ebb4627772101cf8f42e64fee6ab2cf7
Marc Davies <marcdaviesriot@gmail.com>
2019-06-14 17:30:41 -0700

    add show-list component and move list functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { Show } from './models/show.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  shows: Show[] = [
+  selectedShow = null;
+  masterShowList: Show[] = [
     new Show('NOFX', 'Portland Meadows', '07/12/2019 | 08:00 pm', 45, '21+'),
     new Show('Nile', 'Hawthorne Theater', '11/22/2019 | 07:30 pm', 25, 'All Ages'),
     new Show('Gojira', 'Roseland Theater', '07/29/2019 | 08:30 pm', 25, 'All Ages'),
   ];
-  selectedShow = null;
   editShow(clickedShow) {
     this.selectedShow = clickedShow;
   }

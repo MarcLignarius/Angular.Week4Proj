5ba7e9774d8d55d3fa1653498d3b7a78e859c29e
Marc Davies <marcdaviesriot@gmail.com>
2019-06-14 18:05:02 -0700

    separate show edit component from root template

--- src/app/show-edit/show-edit.component.ts
+++ src/app/show-edit/show-edit.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Show } from '../models/show.model';
 
 @Component({
   selector: 'app-show-edit',
   templateUrl: './show-edit.component.html',
   styleUrls: ['./show-edit.component.css']
 })
-export class ShowEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ShowEditComponent {
+  @Input() childSelectedShow: Show;
+  @Output() clickedDone = new EventEmitter();
+  finishedEditing() {
+    this.clickedDone.emit();
   }
 
+  constructor() { }
 }

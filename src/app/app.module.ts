4e169614b2ddead98a54044ce70c5d8047b79952
Marc Davies <marcdaviesriot@gmail.com>
2019-06-21 13:45:34 -0700

    add imports and declarations for Firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }  from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { ShowDetailComponent } from './show-detail/show-detail.component';
+import { ShowEditComponent } from './show-edit/show-edit.component';
 import { ShowListComponent } from './show-list/show-list.component';
 import { ShowNewComponent } from './show-new/show-new.component';
-import { ShowEditComponent } from './show-edit/show-edit.component';
-import { ShowDetailComponent } from './show-detail/show-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FilterPipe } from './filters.pipe';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShowDetailComponent,
+    ShowEditComponent,
     ShowListComponent,
     ShowNewComponent,
-    ShowEditComponent,
-    ShowDetailComponent
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

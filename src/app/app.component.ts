eb5faba17fd275c2681f5a920f88cf45cba07a31
Marc Davies <marcdaviesriot@gmail.com>
2019-06-21 09:48:37 -0700

    add more bands to show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import { Show } from './models/show.model';
 export class AppComponent {
   selectedShow = null;
   masterShowList: Show[] = [
-    new Show('NOFX', 'Portland Meadows', '07/12/2019 | 08:00 pm', 45, '21+'),
-    new Show('Nile', 'Hawthorne Theater', '11/22/2019 | 07:30 pm', 25, 'All Ages'),
-    new Show('Gojira', 'Roseland Theater', '07/29/2019 | 08:30 pm', 25, 'All Ages'),
+    new Show('NOFX, Bad Religion, MxPx', 'Portland Meadows', '07/12/2019 | 08:00 pm', 45, '21+'),
+    new Show('Nile, Terrorizer', 'Hawthorne Theater', '11/22/2019 | 07:30 pm', 25, 'All Ages'),
+    new Show('Gojira, TBA', 'Roseland Theater', '07/29/2019 | 08:30 pm', 25, 'All Ages'),
   ];
   editShow(clickedShow) {
     this.selectedShow = clickedShow;

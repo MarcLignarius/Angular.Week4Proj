276a39637ec77ad23ad129e666ca113774394ca3
Marc Davies <marcdaviesriot@gmail.com>
2019-06-21 12:26:23 -0700

    add show-new component which will handle adding new shows

--- src/app/show-new/show-new.component.ts
+++ src/app/show-new/show-new.component.ts
@@ -1,16 +1,20 @@
-import { Component, Output, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShowService } from '../show.service';
 import { Show } from '../models/show.model';
 
 @Component({
   selector: 'app-show-new',
   templateUrl: './show-new.component.html',
-  styleUrls: ['./show-new.component.css']
+  styleUrls: ['./show-new.component.css'],
+  providers: [ShowService]
 })
-export class ShowNewComponent implements OnInit {
 
-  constructor() { }
+export class ShowNewComponent {
+  constructor(private showService: ShowService) { }
 
-  ngOnInit() {
+  submitForm(flyer: string, bands: string, venue: string, date: string, price: number, ageLimit: string, category: string) {
+    var newShow: Show = new Show(flyer, bands, venue, date, price, ageLimit, category);
+    this.showService.addShow(newShow);
   }
 
 }

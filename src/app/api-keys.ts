c95d43cb936d2428ab79e4b936a6fd3ea6b03f70
Marc Davies <marcdaviesriot@gmail.com>
2019-06-21 15:15:43 -0700

    install firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyDimY3EJoT3F7LpiG7aJS-T1QM4NH3nJjw",
+    authDomain: "pdxshowguide.firebaseapp.com",
+    databaseURL: "https://pdxshowguide.firebaseio.com",
+    projectId: "pdxshowguide",
+    storageBucket: "",
+    messagingSenderId: "187708224329",
+    appId: "1:187708224329:web:9bd8ed97e6f78231"
+};

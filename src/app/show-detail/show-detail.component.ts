ccabd0793d9ef1f0eecd16d6bf515c92b3f97e0d
Marc Davies <marcdaviesriot@gmail.com>
2019-06-21 13:20:21 -0700

    populate show-detail component to handle the display of a show's information

--- src/app/show-detail/show-detail.component.ts
+++ src/app/show-detail/show-detail.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Show } from '../models/show.model';
+import { ShowService } from '../show.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-show-detail',
   templateUrl: './show-detail.component.html',
-  styleUrls: ['./show-detail.component.css']
+  styleUrls: ['./show-detail.component.css'],
+  providers: [ShowService]
 })
 
 export class ShowDetailComponent implements OnInit {
+  showId: string;
+  showToDisplay;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private showService: ShowService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.showId = (urlParameters['id']);
+    });
+    this.showService.getShowById(this.showId).subscribe(dataLastEmittedFromObserver => {
+      this.showToDisplay = new Show (
+        dataLastEmittedFromObserver.flyer,
+        dataLastEmittedFromObserver.bands,
+        dataLastEmittedFromObserver.venue,
+        dataLastEmittedFromObserver.date,
+        dataLastEmittedFromObserver.price,
+        dataLastEmittedFromObserver.ageLimit,
+        dataLastEmittedFromObserver.category
+      )
+    })
+  }
+
+  goBackToShop() {
+    this.router.navigate(['']);
   }
 
 }

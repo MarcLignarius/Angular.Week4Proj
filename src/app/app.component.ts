a8af88e7177721dca2996c2d2f3c39bd1c073394
Marc Davies <marcdaviesriot@gmail.com>
2019-06-21 13:40:20 -0700

    refactor code for Firebase implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Show } from './models/show.model';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +6,5 @@ import { Show } from './models/show.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selectedShow = null;
-  masterShowList: Show[] = [
-    new Show('flyer.jpg','NOFX, Bad Religion, MxPx', 'Portland Meadows', '07/12/2019 | 08:00 pm', 45, '21+'),
-    new Show('flyer.jpg', 'Nile, Terrorizer', 'Hawthorne Theater', '11/22/2019 | 07:30 pm', 25, 'All Ages'),
-    new Show('flyer.jpg', 'Gojira, TBA', 'Roseland Theater', '07/29/2019 | 08:30 pm', 25, 'All Ages'),
-  ];
-  editShow(clickedShow) {
-    this.selectedShow = clickedShow;
-  }
-  finishedEditing() {
-    this.selectedShow = null;
-  }
+  title = 'PDX Show Guide';
 }
